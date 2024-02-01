fc67c5df119542ee67c7ae801c08a45870d28809
shripatil1999 <shripatilkarle@gmail.com>
2024-02-01 18:37:22 +0530

    new

--- src/frontend/dashboard/MeetingMinutes/MeetHistory.js
+++ src/frontend/dashboard/MeetingMinutes/MeetHistory.js
@@ -8,14 +8,40 @@ const MeetHistory = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedMeeting, setSelectedMeeting] = useState(null);
 
-  const handleTabClick = (date) => {
-    setSelectedDate(date);
-    setSelectedMeeting(null); // Reset selected meeting when changing the date
-  };
+  const handleTabClick = (selectedDate) => {
+    // Find the corresponding date object in tasksData
+    const dateObject = tasksData.dates.find((date) => {
+      const formattedDate = new Date(date.date).toISOString().split("T")[0];
+      return formattedDate === selectedDate.toISOString().split("T")[0];
+    });
 
+    if (dateObject && dateObject.meetings && dateObject.meetings.length > 0) {
+      // Set selectedMeeting to the first meeting of the selected date
+      const firstMeeting = dateObject.meetings[0];
+      // console.log("First Meeting:", firstMeeting);
+      setSelectedMeeting(firstMeeting);
+    } else {
+      // If there are no meetings, set selectedMeeting to null or handle it accordingly
+      setSelectedMeeting(null);
+    }
+  };
   const handleTodayClick = () => {
     setSelectedDate(new Date());
-    setSelectedMeeting(null);
+    // Find the corresponding date object in tasksData
+    const dateObject = tasksData.dates.find((date) => {
+      const formattedDate = new Date(date.date).toISOString().split("T")[0];
+      return formattedDate === selectedDate.toISOString().split("T")[0];
+    });
+
+    if (dateObject && dateObject.meetings && dateObject.meetings.length > 0) {
+      // Set selectedMeeting to the first meeting of the selected date
+      const firstMeeting = dateObject.meetings[0];
+      // console.log("First Meeting:", firstMeeting);
+      setSelectedMeeting(firstMeeting);
+    } else {
+      // If there are no meetings, set selectedMeeting to null or handle it accordingly
+      setSelectedMeeting(null);
+    }
   };
 
   return (
@@ -47,10 +73,11 @@ const MeetHistory = () => {
             {tasksData.dates.map((date) => (
               <div key={date.date}>
                 <button
-                  className={`w-full text-left px-4 py-2 ${selectedDate.toISOString().split("T")[0] === date.date
-                      ? "bg-gray-100 shadow-sm"
+                  className={`w-full text-left px-4 py-2 ${
+                    selectedDate.toISOString().split("T")[0] === date.date
+                      ? ""
                       : ""
-                    }`}
+                  }`}
                   onClick={() => handleTabClick(new Date(date.date))}
                 >
                   {date.date}
@@ -61,10 +88,11 @@ const MeetHistory = () => {
                   {date.meetings.map((meeting, index) => (
                     <div
                       key={index}
-                      className={`text-lg font-semibold p-3 text-center cursor-pointer border-1 my-2 ${selectedMeeting === meeting
+                      className={`text-lg font-semibold p-3 text-center cursor-pointer border-1 my-2 ${
+                        selectedMeeting === meeting
                           ? "bg-[#f1f5f9] shadow-sm"
                           : ""
-                        }`}
+                      }`}
                       onClick={() => setSelectedMeeting(meeting)}
                     >
                       <p className="text-left text-gray-800"> {meeting.name}</p>
@@ -82,25 +110,40 @@ const MeetHistory = () => {
         {/* ************************ DISPLAY *********************************** */}
         {selectedMeeting && (
           <div className="w-3/4 p-4">
-          <div className="flex justify-between">
-            <p>Date: {selectedDate.toLocaleDateString()}</p>
-            <p>Duration of the Meeting: 4 Hours 30 Mins.</p>
+            <div className="flex justify-between">
+              <p>Date: {selectedDate.toLocaleDateString()}</p>
+              <p>Duration of the Meeting: 4 Hours 30 Mins.</p>
+            </div>
+            <div className="flex justify-between my-2">
+              <p className="">Meet Code: {selectedMeeting.meetID}</p>
+              <p>Meeting held at: {selectedMeeting.location}</p>
+            </div>
+            <h2 className="text-xl font-bold my-3">
+              {selectedMeeting && ` ${selectedMeeting.name}`}{" "}
+            </h2>
+            <div className="flex justify-between">
+              <div className="chairing">
+                <p>Chairing By:</p>
+
+                {selectedMeeting.chairing.map((chair, index) => (
+                  <div className="flex">
+                  <span key={index}>{chair}</span>
+                  </div>
+                ))}
+                
+              </div>
+            </div>
+
+            <ul>
+              {selectedMeeting &&
+                selectedMeeting.tasks.map((task, index) => (
+                  <React.Fragment key={index}>
+                    <li className="mb-2">{task.uid}</li>
+                    <li className="mb-2">{task.description}</li>
+                  </React.Fragment>
+                ))}
+            </ul>
           </div>
-                        <p>Meet Code: {selectedMeeting.meetID}</p>
-          <h2 className="text-xl font-bold my-3">
-            {" "}
-            {selectedMeeting && ` ${selectedMeeting.name}`}
-          </h2>
-          <ul>
-            {selectedMeeting &&
-              selectedMeeting.tasks.map((task, index) => (
-                <React.Fragment key={index}>
-                  <li className="mb-2">{task.uid}</li>
-                  <li className="mb-2">{task.description}</li>
-                </React.Fragment>
-              ))}
-          </ul>
-        </div>
         )}
       </div>
     </GlobalLayout>
